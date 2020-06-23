94bcbb75aa6bbd7c77316d8249e0876dd36c44c6
Léonard <epoc4oftime@gmail.com>
2020-06-23 14:53:27 +0200

    Setup default Folders etc.

---
+++ Kubika_Release/Assets/Scripts/Enums/CubeTypes.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CubeTypes
+{
+    None = 0,
+
+}
---
+++ Kubika_Release/Assets/Scripts/_Leo/Gameplay/Grid.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kubo : MonoBehaviour
+{
+    public const int gridSize = 12;
+    public const int gridLength = gridSize*gridSize*gridSize;
+
+    public KuboNode[] kuboGrid = new KuboNode[gridLength];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
